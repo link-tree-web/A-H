1096fd06e37c23fc06009f9792cbd20cd298383b
link-tree-web <okgithub03@gmail.com>
2026-08-16 02:51:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var links = document.querySelectorAll('.index-row');
+  links.forEach(function (link) {
+    link.addEventListener('click', function () {
+    });
+  });
+});
